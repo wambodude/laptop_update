f3193ef130ba4d30ec3999586889b3f6ec02fcc3
Tim Krebs <tim.krebs@alexanderthamm.com>
2023-10-24 22:28:31 +0200

    More informatino

--- install_software.sh
+++ install_software.sh
@@ -3,9 +3,11 @@
 # wsl --install ubuntu
 # add user schueler with pw 1234
 # go into docker desktop settings -> set dark, disable statistics and auto startup as well as under resources/wsl integration activate the new ubuntu ressource
+# open terminal application in windows -> go to settings and set ubuntu to default
 
 # update everything && install stuff for zsh, zsh itself and oh-my-zsh
 apt update && apt upgrade -y && apt install git wget zsh -y && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# you have to push y to set zsh as default terminal now
 
 # install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -16,3 +18,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # replace values in configuration
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 sed -i -E 's/(^plugins=\(.*)\)/\1 zsh-syntax-highlighting)/g' ~/.zshrc
+
+# restart the terminal by sourcing the configuration file
+source ~/.zshrc
+
+# the p10k configuration wizard now opens. press the following buttons
+# nnny21321121422n1y
