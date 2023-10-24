40ef1c9564aa07086469725d2df650c1e579b1b5
Tim Krebs <tim.krebs@alexanderthamm.com>
2023-10-24 22:03:31 +0200

    Script and fastapi solution

---
+++ install_software.sh
@@ -0,0 +1,18 @@
+# update everything
+apt update && apt upgrade -y
+
+# install stuff for zsh
+apt install git wget zsh
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# replace values in configuration
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+sed -i -E 's/(^plugins=\(.*)\)/\1 zsh-syntax-highlighting)/g' ~/.zshrc
